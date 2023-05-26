a6367adf154bc253140b029fdb3ddd57fcbc5308
Joanne Imbisi <evizengwa@yahoo.com>
2023-05-25 22:56:46 -0700

    Create components file

---
+++ client/src/components/camerasReducer.js
@@ -0,0 +1,21 @@
+const initialState = [
+    {
+        id: 1,
+        title: "How to train a dragon"
+    },
+    {   id:2,
+        title: "Hello"
+    },
+    {   
+        id:3,
+        title: "Meemaw"
+    }
+
+]
+
+
+const camerasReducer = (state=initialState, action) => {
+    return state
+}
+
+export default camerasReducer
