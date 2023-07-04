bfbf71f77bdf244c21d52c1b08904dea6223ea9d
Joanne Imbisi <evizengwa@yahoo.com>
2023-07-04 12:32:06 -0700

    Modify Orders

--- client/src/MyOrders.js
+++ client/src/MyOrders.js
@@ -1,1 +1,27 @@
+import { useSelector } from "react-redux";
+import Order from "./Order";
+
+
+
+const MyOrders = () => {
+    
+    const orders = useSelector((store) => store.ordersReducer);
  
+    
+
+    const data = orders.map((order) => { 
+        return <Order key={order.id} order={order} />
+    });
+
+    console.log('data', data) // [<Order key='1' />, <Order key='3' />, <Order />]
+    
+    console.log(orders)
+    return(
+        <>
+        <h2> Order History</h2>
+        {data}
+      </>
+    );
+};
+
+export default MyOrders;
